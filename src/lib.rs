d434c09d191a158055938aee250c39bd3eefcf1d
Charley Chen <hintcnuie@sina.com>
2024-02-17 00:25:00 +0800

    version 0.1.3, add dom operation

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,22 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, This is Charley's Rust code  of wasm format!");
+    alert("你好, 这里是 Charley的 Wasm示例代码， 由Rust 代码 经过wasm-pack 转译而成");
+}
+// Called by our JS entry point to run the example
+#[wasm_bindgen(start)]
+fn run() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("你好, 这里是 Charley的 Wasm示例代码， 由Rust 代码 经过wasm-pack 转译而成"));
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
