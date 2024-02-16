0d306059f42bda07dddcfcaf4b4a53da4e3e85d5
chenzw <developer@deltac.com.cn>
2024-02-16 21:10:49 +0800

    versioni 0.1.0

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, This is Charley's Rust code  of wasm format!");
+}
