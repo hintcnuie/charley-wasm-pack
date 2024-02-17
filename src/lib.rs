f745b27f8c39c72444612ab03d8e555d33714534
Charley Chen <hintcnuie@sina.com>
2024-02-17 22:08:48 +0800

    update package version, add canvas function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod utils;
-
+use std::f64;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -24,7 +24,43 @@ fn run() -> Result<(), JsValue> {
     let val = document.create_element("p")?;
     val.set_text_content(Some("你好, 这里是 Charley的 Wasm示例代码， 由Rust 代码 经过wasm-pack 转译而成"));
 
-    body.append_child(&val)?;
+    body.append_child(&val);
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
 
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
     Ok(())
 }
